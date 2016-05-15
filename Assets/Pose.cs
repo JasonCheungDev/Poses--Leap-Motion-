ec6b2cedf761583b32bb84d73a0b82bd8ba96116
Jason Cheung <jayson.cheung@hotmail.com>
2016-05-15 15:29:30 -0700

    First commit
    Contains the basic gist of saving and loading poses.

---
+++ Assets/Pose.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* class used to save and read pose data */
+[System.Serializable]
+public class Pose {
+
+	public float[] distances;		// Distance to each joint, specified by GetComponents<Transform>()
+	public bool directionMatters = false;	// Determines if the orientation of the pose matters, or if the pose can be seen/done in any direction.
+	public Vector3 scale;		    // LocalScale of the saved object (used for distance amplification) 
+
+	public Pose() {}
+
+	public Pose(float[] distances, Vector3 scale = new Vector3()) {
+		this.distances = distances;
+		this.scale = scale;
+	}
+
+	public void Save() {
+		if (distances.Length == 0) {
+			Debug.Log ("Size of distances is 0, error!");
+			return;
+		}
+	}
+
+	public string Print() {
+		string s = "";
+		foreach (float f in distances) {
+			s += f + ",";
+		}
+		s += directionMatters;
+		s += scale;
+		return s;
+	}
+}
---
+++ Assets/PoseController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Xml.Serialization;
+
+/* A script used to save, load, and create poses */
+public class PoseController : MonoBehaviour {
+
+	public float poseBuffer = 10f;
+
+	private Transform[] parts;	// transforms of all child parts + this transform
+
+	// Use this for initialization
+	void Start () {
+		parts = GetComponentsInChildren<Transform> ();	// Note: first one is always this 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void save() {
+		// Create pose object 
+		float[] distances = new float[parts.Length];
+		for (int i = 0; i < parts.Length; ++i) {
+			distances [i] = Vector3.Distance (parts [0].position, parts [i].position);
+		}
+		Pose p = new Pose (distances, transform.localScale);
+		Debug.Log (p.Print());
+		// Serialize object 
+		System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer (p.GetType ());
+
+		// Write to file system 
+		string fileName = "rock.pose";
+		if (File.Exists (fileName)) {
+			Debug.Log (fileName + " already exists, aborting");
+			return; 
+		}
+		using (StreamWriter sw = new StreamWriter (fileName)) { // using keyword closes the stream writer 
+			// Writing data 
+			serializer.Serialize(sw, p);
+			sw.WriteLine();
+		}
+
+		Debug.Log ("Saved pose!");
+	}
+
+	public void load() {
+		// Write to file system 
+		string fileName = "rock.pose";
+		if (!File.Exists (fileName)) {
+			Debug.Log (fileName + " doesn't exist, aborting");
+			return; 
+		}
+			
+		Pose p = new Pose ();
+		XmlSerializer serializer = new XmlSerializer(p.GetType());
+
+		using (StreamReader sr = new StreamReader(fileName)) { // using keyword closes the stream writer 
+			p = (Pose)(serializer.Deserialize (sr));
+		}	
+
+		Debug.Log (p.Print ());
+
+		Debug.Log ("Loaded pose!");
+	}
+
+	public void create() {
+
+	}
+}
